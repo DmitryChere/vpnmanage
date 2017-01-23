75372d4c80338697f437082e964d6ac8e3c48ad4
Dmitry Ch <dmytro.chr.ysbm@gmail.com>
2017-01-23 14:19:51 +0200

    upd

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -181,10 +181,10 @@
                         {{--LIST--}}
                     </tbody>
                 </table>
+            </div>
         </div>
 
         <script src="/js/jquery.js"></script>
-        <script src="/js/jquery.validate.min.js"></script>
         <script src="/js/main.js"></script>
     </body>
 </html>
